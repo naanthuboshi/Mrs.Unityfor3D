9f14b4c425338dd868382a819172694752928c9a
山口奈々 <7stars.0296@gmail.com>
2018-02-05 20:19:50 +0900

    NewGame
    最新版です。

---
+++ Assets/DestroyControler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyControler : MonoBehaviour {
+
+	//ゲームオーバを表示するテキスト
+	private GameObject MainCamera;
+
+
+
+	// Use this for initialization
+	void Start () {
+		//シーン中のMainCameraオブジェクトを取得
+		this.MainCamera = GameObject.Find("Main Camera");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	//カメラのZ座標と障害物のZ座標の差でかめらのZ座標が大きい場合
+		if (this.transform.position.z < this.MainCamera.transform.position.z) {
+	//障害物は破棄する
+			Destroy(gameObject,0.0f);
+		}
+	}
+}
